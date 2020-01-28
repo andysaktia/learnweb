ed02242a6c758d7bb0fa34b2ffc6b71a2e3333e5
andysaktia <37647600+andysaktia@users.noreply.github.com>
2020-01-28 10:45:36 +0700

    Update footerYear.js

--- footer/footerYear.js
+++ footer/footerYear.js
@@ -1,6 +1,4 @@
 // i wanna to make footter year automatic update
+// 2019 can be change with year of creating or started copyright
 
-
-2019
-
-<script>new Date().getFullYear()>2019&&document.write("-"+new Date().getFullYear());</script>
+2019<script>new Date().getFullYear()>2019&&document.write("-"+new Date().getFullYear());</script>
